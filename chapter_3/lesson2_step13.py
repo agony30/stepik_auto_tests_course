d10140444aab0eff15da8b64331d1c337e4b371f
agony30 <agony30@yandex.ru>
2022-07-20 22:18:38 +0500

    Добавление всех выполненных домашек на GitHub

---
+++ chapter_3/lesson2_step13.py
@@ -0,0 +1,75 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+class TestAbs(unittest.TestCase):
+    def test_page1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.first')
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.second')
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.third')
+        input3.send_keys("Smolensk@yandex.ru")
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text,
+                         "Текст не совпал в 1 тесте")
+
+        time.sleep(4)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+    def test_page2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.first')
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.second')
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.third')
+        input3.send_keys("Smolensk@yandex.ru")
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text,
+                         "Текст не совпал во 2 тесте")
+
+        time.sleep(4)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
